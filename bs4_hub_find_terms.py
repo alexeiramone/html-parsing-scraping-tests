883c38960080cbae2eaeb807d9d4a3d02d71d55a
Alexei Martchenko <alexei@martchenko.com.br>
2015-03-28 20:07:04 -0300

    find_terms como função linkify()

--- bs4_hub_find_terms.py
+++ bs4_hub_find_terms.py
@@ -3,6 +3,8 @@ import sys, re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+from bs4 import BeautifulSoup
+
 html_doc = """
 <h2>Heading2</h2>
 <p>Texto como viria do TinyMCE. Preciso enfiar um link para o termo Salame Amarelo, mas não posso linkar o outro termo que já faz parte de um elemento A</p>
@@ -12,73 +14,48 @@ html_doc = """
 <table><tr><td><div><span><b>Esse Salame Amarelo tem que retornar</b></span></div></td></tr></table>
 """
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(html_doc, "html.parser")
 
-texto_encontrar = u"Salame Amarelo"
+def linkify(html_doc, texto_encontrar, link):
+    soup = BeautifulSoup(html_doc, "html.parser")
+    a_nodes_with_text = soup('a', text=texto_encontrar)
+    cool_nodes = set()
 
-def get_nodes_not_parent_of(soupnode, tag):
-    good = set()
-    for tx in soupnode.find_all(tag):
-        pai = tx.parent
-        pai_direto = pai
-        print tx, pai, ':',
-        good.add(pai_direto)
-        while pai:
-            print pai.name,
-            if pai.name == 'a':
-                good.remove(pai_direto)
-                print '|'
-                break
-            pai = pai.parent
-    return good
+    for node in soup.find_all(True):
+        # print node.name
+        if node.string:
+            if node.name != 'a':
+                text_find_pos = unicode(node.string).find(texto_encontrar)
+                if text_find_pos >= 0:
+                    inside_a = 'a' in [n.name for n in node.parents]
+                    if not inside_a:
+                        cool_nodes.add(node)
+                        # print node.name, node.string, text_find_pos, inside_a, 'added'
+                    if node.parent in cool_nodes:
+                        cool_nodes.remove(node.parent)
+                        # print node.parent, 'removed'
+                        # print node.name, node.string, '::', text_find_pos, inside_a
 
-def find_nodes(text):
-    good = set()
-    for tx in soup.find_all(True):
-        print tx.name
+    # print len(cool_nodes)
+    # print cool_nodes
+    if type(link) in (str, unicode):
+        new_tag = soup.new_tag("a", href=link)
+    elif type(link) is dict:
+        new_tag = soup.new_tag("a", **link)
+        # new_tag['data-hub'] = 1
+    new_tag.string = texto_encontrar
 
-# print find_nodes(texto_encontrar)
-# print get_nodes_not_parent_of(texto_encontrar)
+    # print unicode(new_tag)
 
+    for sel_node in cool_nodes:
+        # print '----', sel_node.contents
+        node_replace = unicode(sel_node.string).replace(texto_encontrar, unicode(new_tag))
+        node_replace = BeautifulSoup(node_replace, "html.parser")
+        sel_node.string.replace_with(node_replace)
+        # print '----', node_replace
 
-# print soup.find_all('a')
-# print get_nodes_not_parent_of(soup, 'a')
+    # print soup
+    return soup
 
-a_nodes_with_text = soup('a', text=texto_encontrar)
-
-cool_nodes = set()
-
-for node in soup.find_all(True):
-    # print node.name
-    if node.string:
-        if node.name != 'a':
-            text_find_pos = unicode(node.string).find(texto_encontrar)
-            if text_find_pos >= 0:
-                inside_a = 'a' in [n.name for n in node.parents]
-                if not inside_a:
-                    cool_nodes.add(node)
-                    # print node.name, node.string, text_find_pos, inside_a, 'added'
-                if node.parent in cool_nodes:
-                    cool_nodes.remove(node.parent)
-                    # print node.parent, 'removed'
-                    # print node.name, node.string, '::', text_find_pos, inside_a
-
-print len(cool_nodes)
-print cool_nodes
-
-new_tag = soup.new_tag("a", href="/hub/salame-amarelo.html")
-new_tag['data-hub'] = 1
-new_tag.string = texto_encontrar
-
-print unicode(new_tag)
-
-for sel_node in cool_nodes:
-    print '----', sel_node.contents
-    node_replace = unicode(sel_node.string).replace(texto_encontrar, unicode(new_tag))
-    node_replace = BeautifulSoup(node_replace, "html.parser")
-    sel_node.string.replace_with(node_replace)
-    print '----', node_replace
-
-print soup
-
+# print linkify(html_doc, u"Salame Amarelo", "/bunda.html")
+print linkify(html_doc, u"Salame Amarelo", {'href':'ostra.html', 'target':'_blank', 'data-hub':1})
+print linkify(html_doc, u"Bunda Verde", "/000.html")
