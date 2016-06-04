c168f6c4be3f60672364f4f73a164431d16db576
Alexei Martchenko <alexei@martchenko.com.br>
2016-06-03 21:42:30 -0300

    Fixes que refiz depois

--- homelinks/uol.py
+++ homelinks/uol.py
@@ -9,13 +9,13 @@ from bs4 import BeautifulSoup
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-# h = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
-# r = requests.get("http://www.uol.com.br/", headers=h)
-# html = r.text
-# print r.status_code, r.headers
+h = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
+r = requests.get("http://www.uol.com.br/", headers=h)
+html = r.text
+print r.status_code, r.headers
 
-# with codecs.open('uol.com.br.html','wb') as f:
-#   f.write(html)
+with codecs.open('uol.com.br.html','wb') as f:
+  f.write(html)
 
 with codecs.open('uol.com.br.html') as f:
     html = f.read()
@@ -26,10 +26,40 @@ links = soup.find_all('a', attrs={'data-metrics': True, 'href':True})
 
 print len(links), "links no total."
 
-for i,link in enumerate(links):
-    print i, link.get('data-metrics'), link.get('href'), link.get('name')
+resultado = dict(pos=None, texto=None, metrics=None, href=None, name=None)
+
+for resultado['pos'],link in enumerate(links):
+    resultado['metrics'] = link.get('data-metrics')
+    resultado['href'] = link.get('href')
+    resultado['name'] = link.get('name')
+
+    # Link com classe "link", só se for um.
     x = link.find(attrs={'class': 'link'})
     if x:
-        print list(x.stripped_strings)
-    # for xs in link.stripped_strings:
-    #     print repr(xs)
+        x = list(x.stripped_strings)
+        if len(x) == 1 and len(x[0]) > 3:
+            resultado['texto'] = x[0]
+
+    x = link.find(attrs={'class': 'chamada'})
+    if x:
+        x = list(x.stripped_strings)
+        if len(x) == 1 and len(x[0]) > 3:
+            resultado['texto'] = x[0]
+
+    if not resultado['texto']:
+        x = link.find(attrs={'class': 'text'})
+        if x:
+            x = list(x.stripped_strings)
+            if len(x) == 1 and len(x[0]) > 3:
+                resultado['texto'] = x[0]
+
+    if not resultado['texto']:
+        x = list(link.stripped_strings)
+        if len(x) == 1 and len(x[0]) > 3:
+            resultado['texto'] = x[0]
+
+    if not resultado['texto']:
+        print link
+        for xs in link.stripped_strings:
+            print repr(xs)
+    # print resultado
